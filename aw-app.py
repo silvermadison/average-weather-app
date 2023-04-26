abbaca6dc7effd5b3ba880cbe7acec8d796de85a
Ubuntu <ubuntu@msilver-vm.js2local>
2023-04-26 16:30:41 +0000

    new changes

--- aw-app.py
+++ aw-app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 import requests
 import redis
 import json
@@ -133,6 +133,47 @@ def get_yr_high(loc_num):
         sum = sum+x
     yr_highT = sum/12
     return str(yr_highT)
+
+@app.route('/locations/<int:loc_num>/high-month/plot', methods=['GET', 'POST', 'DELETE'])
+def get_month_high(loc_num):
+    '''
+    This function will get, post, or delete an image from the dataset based on the method verb provided by the user.
+    Returns:
+        image(png): histogram of the average high temperatures by month for a given location num/ID
+        OR
+        (str): stating the the image in the database has been posted or deleted
+    '''
+    if request.method == 'GET':
+        if rd_img.exists('highT_avs'):
+            with open('./highT_avs.png', 'wb') as f:
+                f.write(rd_img.get('highT_avs'))
+            return send_file('./highT_avs.png', minetype='highT_avs/png', as_attachment=True)#send image to user
+        else:
+            return 'there is no image in the database'
+
+    elif request.method == 'POST':
+        for item in rd.keys():
+            alldata.append()json.loads(rd.get(item)))
+        if loc_num >= len(alldata):
+            return "Error: Location value is not in the data set" , 400
+        highT = get_month_high(loc_num)
+        #bar_labels = ['Jaunary','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September','October', 'November', 'December' ]
+        plt.hist(highT, 5)
+        title = f'Average High Temperatures in {alldata[loc_num]["city"]}'
+        plt.title(title)
+        plt.xlabel('Months')
+        plt.ylabal('Temperature in Celsius')
+        plt.savefig('./highT_avs.png')
+        filebytes = open('./highT_avs.png','rb').read()
+        rd_img.set('highT_avs', filebytes)
+        return 'the image has been uploaded to the database.\n'
+
+    elif request.method == 'DELETE':
+        rd_img.flushdb()
+        return 'the image has been deleted from the database'
+    
+    else:
+        return 'error in requested method', 400
 
 #______________________end of routes________________________
 if __name__ == '__main__':
